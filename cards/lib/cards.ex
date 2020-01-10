387b30abaf6064d74a9bccef3c332ed323138420
Deivid Veloso <veloso.deivid@gmail.com>
2020-01-10 15:25:50 -0300

    learning pattern matching

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -16,13 +16,9 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for value <- values do 
-      for suit <- suits do
-        "#{value} of #{suit}"
-      end
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
-
-    List.flatten(cards)
   end
 
   def shuffle(deck) do
@@ -31,6 +27,10 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 
 end
