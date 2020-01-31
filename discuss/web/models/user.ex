086ea59c0b360d1ddbeaa310293ac1040af06832
Deivid Veloso <veloso.deivid@gmail.com>
2020-01-31 16:18:33 -0300

    finish OAuth and init Plugs

---
+++ discuss/web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  # access User Table
+  schema "users" do
+    field(:email, :string)
+    field(:provider, :string)
+    field(:token, :string)
+
+    timestamps()
+  end
+
+  # Generate my commands to run in Database(UPDATE, INSERT, ...)
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
+  end
+end
