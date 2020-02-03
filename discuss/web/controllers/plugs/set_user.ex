6bfddf2d200c37ece6999f903cb7e8b43276cc06
Deivid Veloso <veloso.deivid@gmail.com>
2020-02-02 21:28:12 -0300

    add plug OAuth

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -1,4 +1,24 @@
 defmodule Discuss.Plugs.SetUser do
   import Plug.Conn
   import Phoenix.Controller
+
+
+  def init(_params) do
+    
+  end
+
+  def call(conn, _params) do
+    #get_session coming from controller
+    user_id = get_session(conn, :user_id)
+    IO.inspect(user_id)
+    cond do
+      #assign comming from Plug.Conn
+      user = user_id && Discuss.Repo.get(Discuss.User, user_id) -> 
+        assign(conn, :user, user)
+      true -> 
+        assign(conn, :user, nil)
+    end
+
+  end
+
 end
